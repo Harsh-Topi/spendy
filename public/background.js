cedc038ca3282da5d87be2b0d6a9a179277ee197
Hyrum Nantais <hnantais@uoguelph.ca>
2022-01-08 18:55:27 -0500

    FEATURE - Add shopify item parsing into chrome storage

--- public/background.js
+++ public/background.js
@@ -1,14 +1,14 @@
 chrome.runtime.onInstalled.addListener(async () => {
-    chrome.storage.sync.set({
-        user_info: {
-            first_name: '',
-            email: '',
-        },
-        limits: {
-            daily: -1,
-            weekly: -1,
-            monthly: -1
-        },
-        transaction_info: {}
-    });
+  chrome.storage.sync.set({
+    user_info: {
+      first_name: '',
+      email: '',
+    },
+    limits: {
+      daily: -1,
+      weekly: -1,
+      monthly: -1
+    },
+    transaction_info: {}
+  });
 });
