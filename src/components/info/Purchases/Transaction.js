a80a1833bd41b4a28135ccec0d8f191ce3a065a4
htopiwal <htopiwal@uoguelph.ca>
2022-01-08 16:52:36 -0500

    UI FEATURES: Added limit bubbles andt transaction history UI

---
+++ src/components/info/Purchases/Transaction.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../../../styles/info/Summary.css'
+
+
+const Transaction = ({ purchaseInfo }) => {
+
+  return (
+    <div className="dayHeader"><span className="dateText">test</span><span className="amountText">test</span></div>
+  )
+}
+
+export default Transaction
---
+++ src/components/info/Summary/LimitBubble.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../../../styles/info/Summary.css'
+
+
+const LimitBubble = ({ color, spent, limit, interval }) => {
+  return (
+    <div style={{ background: color }} className="bubble">
+      <span className="bubbleNums">{`$${spent} / $${limit}`}</span>
+      <span className="bubbleText">{interval}</span>
+    </div>
+  )
+}
+
+export default LimitBubble
